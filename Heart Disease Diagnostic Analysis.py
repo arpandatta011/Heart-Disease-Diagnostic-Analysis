3fc7583f938d1f5eb9000488ac9e1b018601dfa2
ArpanDatta002 <93440576+arpandatta11@users.noreply.github.com>
2022-05-13 10:44:12 +0530

    Heart-Disease Diagnostic Project 1

---
+++ Heart Disease Diagnostic Analysis.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+
+
+# In[2]:
+
+
+Heart = pd.read_csv("heart.csv")
+
+
+# In[3]:
+
+
+Heart.head().style.set_properties(**{'background-color':'pink','color':'grey','border-color':'black','font-size':'10pt','width':200})
+
+
+# In[4]:
+
+
+Heart.shape
+
+
+# In[5]:
+
+
+Heart.isna().sum()
+
+
+# In[6]:
+
+
+Heart.columns
+
+
+# In[7]:
+
+
+Heart['sex'].value_counts()
+
+
+# In[8]:
+
+
+output = Heart.groupby('output').size()
+output
+
+
+# In[9]:
+
+
+# numarical value to convart catagorical; value
+
+
+# In[10]:
+
+
+def Heart_disease (row):
+    if row == 0:
+        return 'Absence'
+    elif row == 1:
+        return 'Presence'
+
+
+# In[11]:
+
+
+Heart['Heart_disease'] = Heart['output'].apply(Heart_disease)
+
+
+# In[12]:
+
+
+Hd = Heart.groupby('Heart_disease')['output'].count()
+Hd
+
+
+# In[13]:
+
+
+Heart.head()
+
+
+# In[14]:
+
+
+plt.figure(figsize = (10,9))
+plt.pie(Hd,labels= ['Absence','Presence'],autopct= '%0.1f%%')
+plt.title('Heart Disease Population (%)', fontsize=20)
+plt.show()
+
+
+# In[15]:
+
+
+plt.figure(figsize = (18,9))
+sns.countplot(x = 'age',data = Heart)
+plt.title('Population of age', fontsize=19)
+plt.xlabel('Age',fontsize=18)
+plt.ylabel('Count',fontsize=18)
+plt.show()
+
+
+# In[16]:
+
+
+#Statistical Analysis
+
+Min_Age = Heart['age'].min()
+Max_Age = Heart['age'].max()
+Mean_Age = Heart['age'].mean()
+print('Minimum Age:>',Min_Age)
+print('Maximum Age:>',Max_Age)
+print('Mean Age:>',Mean_Age)
+
+
+# In[17]:
+
+
+#Categorical Analysis
+
+Young_Ages=Heart[(Heart['age']>=29) & (Heart['age']<40)]
+Middle_Ages=Heart[(Heart['age']>=40) & (Heart['age']<55)]
+Senior_Ages=Heart[(Heart['age']>=55)]
+print('Young Ages =>',len(Young_Ages))
+print('Middle Ages =>',len(Middle_Ages))
+print('Senior Ages =>',len(Senior_Ages))
+
+
+# In[18]:
+
+
+#Bar Plot Creation of Age Category using MatplotLib and Seaborn
+plt.figure(figsize = (10,7))
+sns.barplot(x = ['Young_Ages','Middle_Ages','Senior_Ages'], y = [len(Young_Ages),len(Middle_Ages),len(Senior_Ages)],palette='deep',data = Heart)
+plt.title('Age Category', fontsize=17)
+plt.xlabel('Age Range', fontsize=15)
+plt.ylabel('Count', fontsize=15)
+plt.show()
+
+
+# In[24]:
+
+
+#Converting Numerical Data into Categorical Data
+def Sex1 (row):
+    if row == 1:
+        return 'Male'
+    elif row == 0:
+        return 'Female'
+
+
+# In[38]:
+
+
+Heart['Sex1'] = Heart['sex'].apply(Sex1)
+
+
+# In[39]:
+
+
+Heart.head()
+
+
+# In[23]:
+
+
+Heart['Sex1'].value_counts()
+
+
+# In[45]:
+
+
+def age_range (row):
+    if row >=29 & row <40:
+        return 'Young age'
+    if row >=40 & row <55:
+        return 'Middle_age'
+    if row >55:
+        return 'Senior_age'
+
+
+# In[46]:
+
+
+Heart['Age_range'] = Heart['age'].apply(age_range)
+
+
+# In[47]:
+
+
+Heart.head()
+
+
+# In[ ]:
+
+
+
+
